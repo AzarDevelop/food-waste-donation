5a7308c34cbb78d67d4ae2e59b8847614312dd14
Ajmal Khan <9043799792ajmal@gmail.com>
2026-08-01 18:46:28 +0530

    Update Navbar.js

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
+
 const roleHome = {
   restaurant: '/restaurant',
   ngo: '/ngo',
